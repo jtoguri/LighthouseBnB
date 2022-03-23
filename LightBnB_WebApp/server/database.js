92b5bf27eba5d3c1d3e72290579910295112291b
Joshua Toguri <joshua.toguri@gmail.com>
2022-03-23 09:48:29 -0400

    added query to search for a guest's reservation history so they can view it on the web page

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -82,7 +82,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`
+    SELECT *
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2
+    `, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
